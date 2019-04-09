e39fe7f95f486a03fe440426e57894d2e6c831e5
Will Bonnell <wtb3na@virginia.edu>
2019-04-09 16:41:46 -0500

    emily

---
+++ R/tl_emily.R
@@ -0,0 +1,13 @@
+#' tl_emily
+#'
+#'Easter egg giving credit to Emily for the idea for this package :)
+#'
+#' @return emily
+#' @export
+#'
+#' @examples
+#' tl_emily()
+#'
+tl_emily <- function() {
+  print("Hello, Emily!")
+}
