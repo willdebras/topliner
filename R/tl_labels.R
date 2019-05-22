e33e1a544b1fa6ef4a9fd1a6ceeac56b1e542c2f
Will Bonnell <wtb3na@virginia.edu>
2019-05-22 11:02:47 -0500

    adding exhaustive labels function that assigns object to global env to reduce compatibility issues with other functions

--- R/tl_labels.R
+++ R/tl_labels.R
@@ -12,14 +12,29 @@
 #'
 #'
 tl_labels <- function(data, spss = FALSE) {
+  require(stringr)
   if (spss) {
     labels <- attr(data, "variable.labels")
-    tibble(name = tolower(names(labels)),
-           label = labels)
+    labels <- tibble(name = tolower(names(labels)),
+                     label = labels)
   }
   else {
     labels <- sapply(data, function(x) attr(x, "label"))
-    tibble(name = names(labels),
-           label = labels)
+    labels <- tibble(name = names(labels),
+                     label = labels)
   }
+
+  battery_labels <- str_extract(labels$label, "(?<=\\[)(.*?)(?=\\])")
+  question_labels <- str_replace(labels$label, "(?<=\\[)(.*?)(?=\\])", "")
+  question_labels <- gsub("\\[", "", question_labels)
+  question_labels <- gsub("\\]", "", question_labels)
+  question_labels <- str_trim(question_labels)
+
+  labels <- labels %>%
+    cbind(battery_labels) %>%
+    cbind(question_labels)
+
+  data_labels <<- labels
+  #assign("labels", labels, envir = .GlobalEnv)
+
 }
