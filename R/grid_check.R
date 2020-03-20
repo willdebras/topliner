51fb2aa54aafe50a63b3952b7cbae08df41ee9c6
Tomas Okal <tomas.okal@gmail.com>
2020-03-20 14:45:31 -0500

    Function to create single row of check all that apply questions.

---
+++ R/grid_check.R
@@ -0,0 +1,20 @@
+#' grid_check
+#'
+#' This is a helper function to create grid check all that apply question table.
+#'
+#' @param var A variable of interest from survey design.
+#'
+#' @return A data.table output of survey proportions for provided variable.
+#'
+#' @import data.table
+#' @import survey
+#'
+grid_check <- function(var, svy_dt) {
+
+  formula <- paste0("~", "var")
+  survey_output <- as.data.table(svytable(formula = formula, svy_dt, Ntotal = 100))
+  survey_output <- survey_output[, data.table(t(.SD), keep.rownames = TRUE)]
+
+  return(survey_output)
+
+}
