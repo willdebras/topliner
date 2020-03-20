7b66057a309f7ec9f956856dbef20bc6e39359c5
Will Bonnell <wtb3na@virginia.edu>
2020-03-20 16:26:40 -0500

    changing svytable option to Ntotal = 100

--- R/topliner_scratch.R
+++ R/topliner_scratch.R
@@ -2,7 +2,7 @@
 
 data2 <- haven::read_dta("P:\\AP-NORC Center\\Common\\AP Special Projects\\Omnibus\\2019\\05.2019\\Data\\old\\May_completes_clean.dta")
 
-library(dplyr)
+library(magrittr)
 library(survey)
 library(srvyr)
 
@@ -10,8 +10,13 @@ data2 <- as_factor(data2)
 
 topliner::tl_setup(data = data2, caseids = "caseid", weights = "weight_enes", dates = "05/17-20/2019")
 
-tib <- as.data.frame(svytable(~rel3d, tl_df))
+tib <- as.data.frame(svytable(~rel3d, tl_df, Ntotal = 100))
 att <- as.data.frame(as.factor(attributes(data2$rel3d)$labels)) %>%
   `colnames<-`("rel3d")
 
+att$labels <- rownames(att)
+
 tib_2 <- right_join(tib, att)
+
+tib_2$Freq[is.na(tib_2$Freq)] <- 0
+
